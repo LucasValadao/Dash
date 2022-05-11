2c76d623db37477a4582caf16de8085d7aa71178
LucasValadao <33871350+LucasValadao@users.noreply.github.com>
2022-05-11 20:04:34 -0300

    [add] fim da implementação da security e fix dos erros finais

--- src/main/java/com/example/Dash/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/Dash/security/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.Dash.security.config;
 
 
+import com.example.Dash.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,14 +34,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider()
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider provider =
                 new DaoAuthenticationProvider();
-        provider.setPasswordEncoder();
-        provider.setUserDetailsService();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
         return provider;
     }
 }
